6b55a8c45bd05b99df083ac7268db6f85d2b9158
marksprojects23 <markc7856@yahoo.com>
2024-03-12 10:54:53 -0600

    pushing cowboy server app real quick

---
+++ tpf_cowboy/README.md
@@ -0,0 +1,9 @@
+tpf_cowboy
+=====
+
+An OTP application
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ tpf_cowboy/src/tpf_cowboy_app.erl
@@ -0,0 +1,60 @@
+%%%-------------------------------------------------------------------
+%% @doc TrackPackFlow public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(tpf_cowboy_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_Type, _Args) ->
+    Dispatch = cowboy_router:compile([
+        {'_', [
+            % {"/", default_page_h, []},
+            % {"/logn", log_in_h, []}, % this is an example on adding more pages to ur web app.
+            {"/test", hello_h, []}
+        ]}
+    ]),
+
+	PrivDir = code:priv_dir(tpf_cowboy),
+	%tls stands for transport layer security
+        {ok,_} = cowboy:start_tls(https_listener, [
+                  		{port, 443},
+				{certfile, PrivDir ++ "/ssl/fullchain.pem"},
+				{keyfile, PrivDir ++ "/ssl/privkey.pem"}
+              		], #{env => #{dispatch => Dispatch}}),
+	tpf_cowboy_sup:start_link().
+stop(_State) ->
+    ok.
+
+%% internal functions
+
+
+
+
+
+% -behaviour(application).
+
+% -export([start/2, stop/1]).
+
+% start(_StartType, _StartArgs) ->
+%     Dispatch = cowboy_router:compile([
+%             {'_', [
+%                 % {"/", default_page_h, []},
+%                 % {"/logn", log_in_h, []}, % this is an example on adding more pages to ur web app.
+%                 {"/test", hello_h, []}
+%             ]}
+%         ]),
+%         cowboy:start_clear(
+%             my_http_listener,
+%             [{port, 8087}],
+%             #{env => #{dispatch => Dispatch}}
+%         ),
+%     trackpackflow_sup:start_link().
+
+% stop(_State) ->
+%     ok.
+
+% %% internal functions
