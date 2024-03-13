1609584828eaa74816575c528a4f373f10859ebe
xter7856 <markc7856@yahoo.com>
2024-03-13 10:55:53 -0600

    testing http cowboy instead of https since we have haproxy

--- tpf_cowboy/src/tpf_cowboy_app.erl
+++ tpf_cowboy/src/tpf_cowboy_app.erl
@@ -18,14 +18,20 @@ start(_Type, _Args) ->
         ]}
     ]),
 
-	PrivDir = code:priv_dir(tpf_cowboy),
-	%tls stands for transport layer security
-        {ok,_} = cowboy:start_tls(https_listener, [
-                  		{port, 8080},
-				{certfile, PrivDir ++ "fullchain.pem"},
-				{keyfile, PrivDir ++ "privkey.pem"}
-              		], #{env => #{dispatch => Dispatch}}),
-	tpf_cowboy_sup:start_link().
+        cowboy:start_clear(
+            my_http_listener,
+            [{port, 8080}],
+            #{env => #{dispatch => Dispatch}}
+        ),
+    tpf_cowboy_sup:start_link().
+	% PrivDir = code:priv_dir(tpf_cowboy),
+	% %tls stands for transport layer security
+    %     {ok,_} = cowboy:start_tls(https_listener, [
+    %               		{port, 8080},
+	% 			{certfile, PrivDir ++ "fullchain.pem"},
+	% 			{keyfile, PrivDir ++ "privkey.pem"}
+    %           		], #{env => #{dispatch => Dispatch}}),
+	% tpf_cowboy_sup:start_link().
 stop(_State) ->
     ok.
 
