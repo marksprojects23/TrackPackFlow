3095f71b30ba79ba1a9e7899671f0856e938b96f
xter7856 <markc7856@yahoo.com>
2024-03-20 09:44:54 -0600

    pushing some gen server testings

--- tpf_cowboy/src/request_h.erl
+++ tpf_cowboy/src/request_h.erl
@@ -4,5 +4,6 @@
 -export([init/2]).
 
 init(Req, State) ->
+    {ok, Req_body, _End_req} = cowboy_req:read_body(Req),
     Req2 = cowboy_req:reply(200, #{}, <<"Request Req">>, Req),
     {ok, Req2, State}.
---
+++ trackpackflow/src/package_storer.erl
@@ -0,0 +1,179 @@
+%% @author Lee Barney
+%% @copyright 2022 Lee Barney licensed under the <a>
+%%        rel="license"
+%%        href="http://creativecommons.org/licenses/by/4.0/"
+%%        target="_blank">
+%%        Creative Commons Attribution 4.0 International License</a>
+%%
+%%
+-module(tpf_mock).
+-behaviour(gen_server).
+
+
+%% API
+-export([start/0,start/3,stop/0]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server assuming there is only one server started for 
+%% this module. The server is registered locally with the registered
+%% name being the name of the module.
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec start() -> {ok, pid()} | ignore | {error, term()}.
+start() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts a server using this module and registers the server using
+%% the name given.
+%% Registration_type can be local or global.
+%%
+%% Args is a list containing any data to be passed to the gen_server's
+%% init function.
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec start(atom(),atom(),atom()) -> {ok, pid()} | ignore | {error, term()}.
+start(Registration_type,Name,Args) ->
+    gen_server:start_link({Registration_type, Name}, ?MODULE, Args, []).
+
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Stops the server gracefully
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec stop() -> {ok}|{error, term()}.
+stop() -> gen_server:call(?MODULE, stop).
+
+%% Any other API functions go here.
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec init(term()) -> {ok, term()}|{ok, term(), number()}|ignore |{stop, term()}.
+init([]) ->
+        {ok,replace_up}.
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec handle_call(Request::term(), From::pid(), State::term()) ->
+                                  {reply, term(), term()} |
+                                  {reply, term(), term(), integer()} |
+                                  {noreply, term()} |
+                                  {noreply, term(), integer()} |
+                                  {stop, term(), term(), integer()} | 
+                                  {stop, term(), term()}.
+handle_call({storing_package, Package_id, Location_id}, _From, Db_PID) ->
+        case lists:any(fun(A)->A=="" end,[Package_id,Location_id]) of
+            true ->
+                {reply,{fail,empty_key},Db_PID};
+            _ ->
+                % {reply,data_service:store_package(Package_id, Location_id, Db_PID),Db_PID}
+                {reply,{ok,Package_id,Location_id},Db_PID}
+        end;
+
+handle_call({delivering_package,Package_id}, _From, Db_PID) ->
+        case Package_id =:= ("") of
+            true ->
+                {reply,{fail,empty_key},Db_PID};
+            _ ->
+                {reply,data_service:delivered_package(Package_id, Db_PID),Db_PID}
+        end;
+
+handle_call({getting_location,Package_id}, _From, Db_PID) ->
+        case Package_id =:= ("") of
+            true ->
+                {reply,{fail,empty_key},Db_PID};
+            _ ->
+                {reply,data_service:get_location(Package_id,Db_PID),Db_PID}
+        end;
+handle_call(stop, _From, _State) ->
+        {stop,normal,
+                replace_stopped,
+          down}. %% setting the server's internal state to down
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec handle_cast(Msg::term(), State::term()) -> {noreply, term()} |
+                                  {noreply, term(), integer()} |
+                                  {stop, term(), term()}.
+
+handle_cast({updating_location, Data}, Pid) -> 
+    data_service:update_location(Data, Pid),
+    {noreply, Pid};
+
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+
+    
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @end
+-spec handle_info(Info::term(), State::term()) -> {noreply, term()} |
+                                   {noreply, term(), integer()} |
+                                   {stop, term(), term()}.
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec terminate(Reason::term(), term()) -> term().
+terminate(_Reason, _State) ->
+    ok.
+    
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec code_change(term(), term(), term()) -> {ok, term()}.
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+    
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
