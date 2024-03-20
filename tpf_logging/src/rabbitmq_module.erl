dc80af6af6c55d3daca3e164013b53b7261929a1
xter7856 <markc7856@yahoo.com>
2024-03-20 10:50:20 -0600

    more changes

--- tpf_logging/src/rabbitmq_module.erl
+++ tpf_logging/src/rabbitmq_module.erl
@@ -1,6 +1,7 @@
 
 -module(rabbitmq_module).
 -export([start_link/0, send_message/1]).
+-include_lib("amqp_client/include/amqp_client.hrl").
 
 %% Starting the RabbitMQ module
 start_link() ->
@@ -14,20 +15,20 @@ send_message(Message) ->
     gen_server:call(?MODULE, {send_message, Message}).
 
 %% Initialization function for RabbitMQ module
-init([]) ->
-    %% Establish connection and channel
-    {ok, Connection} = amqp_connection:start(#amqp_params_network{host = "localhost", username = "trackpackflow", password = "cse481Barney"}),
-    {ok, Channel} = amqp_connection:open_channel(Connection),
-    %% Declare the queue
-    amqp_channel:call(Channel, #'queue.declare'{queue = <<"trackpackflow">>}),
-    {ok, #state{channel = Channel}}.
+% init([]) ->
+%     %% Establish connection and channel
+%     {ok, Connection} = amqp_connection:start(#amqp_params_network{host = "localhost", username = "trackpackflow", password = "cse481Barney"}),
+%     {ok, Channel} = amqp_connection:open_channel(Connection),
+%     %% Declare the queue
+%     amqp_channel:call(Channel, #'queue.declare'{queue = <<"trackpackflow">>}),
+%     {ok, #state{channel = Channel}}.
 
-%% Handling the message sending request
-handle_call({send_message, Message}, _From, State) ->
-    %% Publish the message to RabbitMQ
-    amqp_channel:call(State#state.channel, #'basic.publish'{exchange = <<"">>, routing_key = <<"trackpackflow">>}, #amqp_msg{payload = Message}),
-    {reply, ok, State}.
+% %% Handling the message sending request
+% handle_call({send_message, Message}, _From, State) ->
+%     %% Publish the message to RabbitMQ
+%     amqp_channel:call(State#state.channel, #'basic.publish'{exchange = <<"">>, routing_key = <<"trackpackflow">>}, #amqp_msg{payload = Message}),
+%     {reply, ok, State}.
 
-%% Cleanup on termination
-terminate(_Reason, _State) ->
-    ok.
+% %% Cleanup on termination
+% terminate(_Reason, _State) ->
+%     ok.
