895486deabae08ea9d006c1c0326c50e419c2583
marksprojects23 <markc7856@yahoo.com>
2024-03-21 11:03:33 -0600

    test if erpc works

--- tpf_cowboy/src/request_h.erl
+++ tpf_cowboy/src/request_h.erl
@@ -7,6 +7,6 @@ init(Req, State) ->
     % Package_id is in binary
     {ok, Package_id, _End_req} = cowboy_req:read_body(Req),
     % erpc:call('storer@business.tpf.markcuizon.com', data_service, store_package, {storing_package, Package_id, Location_id}),
-    erpc:call('storer@business.tpf.markcuizon.com', package_storer, handle_call, {storing_package, Package_id}),
+    % Result = erpc:call('storer@business.tpf.markcuizon.com', data_service, get_location, {storing_package, Package_id}),
     Req2 = cowboy_req:reply(200, #{}, <<"Request Req">>, Req),
     {ok, Req2, State}.
