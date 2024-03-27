bde60c01e938258e725ef26e9653fed11601b7e6
xter7856 <markc7856@yahoo.com>
2024-03-27 10:19:24 -0600

    fixing delivered handler

--- tpf_cowboy/src/delivered_h.erl
+++ tpf_cowboy/src/delivered_h.erl
@@ -8,6 +8,6 @@ init(Req, State) ->
     %  "package_id": _
     %}
     {ok, Req_body, _End_req} = cowboy_req:read_body(Req),
-    erpc:call('storer@business.tpf.markcuizon.com', gen_server, call, [{global, realstorer}, {delivering_package, Req_body}, infinity]),
+    erpc:call('tpf@business.tpf.markcuizon.com', gen_server, call, [{global, realstorer}, {delivering_package, Req_body}, infinity]),
     Req2 = cowboy_req:reply(200, #{}, <<"Deliver_req">>, Req),
     {ok, Req2, State}.
