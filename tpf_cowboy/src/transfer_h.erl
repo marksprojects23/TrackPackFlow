913d095f668f6cebe878e35c49b3b46b5e3d1d0d
Trevin <tr3vinj@gmail.com>
2024-03-27 20:08:07 -0600

    Undoing last thing

--- tpf_cowboy/src/transfer_h.erl
+++ tpf_cowboy/src/transfer_h.erl
@@ -4,33 +4,32 @@
 -export([init/2]).
 
 init(Req, State) ->
-    io:format("Transfer request!"),
     %{
     %  "package_id": _,
     %  "location_id": _
     %}
     {ok, Req_body, _End_req} = cowboy_req:read_body(Req),
-    % Decode the JSON. Using option return_maps returns the JSON as an erlang map.
+    % Decode the JSON. Using option return_maps returns the JSON as an erlang map
     Decoded_req_body_map = jiffy:decode(Req_body, [return_maps]),
     {ok, Location_id} = maps:find(<<"location_id">>, Decoded_req_body_map),
     {ok, Package_id} = maps:find(<<"package_id">>, Decoded_req_body_map),
-%     Test = fun(A) ->
-%         if
-%             is_atom(A) -> 
-%                 io:format("An atom was passed:~n");
-%             is_binary(A) ->
-%                 io:format("A binary was passed:~n");
-%             is_list(A) ->
-%                 io:format("A list was passed (possibly a string):~n");
-%             true -> 
-%                 io:format("Whatever passed is of another type:~n")
-% end end,
-%     Test(Package_id),
-%     io:format(Package_id),
-%     io:format("~n"),
-%     Test(Location_id),
-%     io:format(Location_id),
-%     io:format("~n"),
+    Test = fun(A) ->
+        if
+            is_atom(A) -> 
+                io:format("An atom was passed:~n");
+            is_binary(A) ->
+                io:format("A binary was passed:~n");
+            is_list(A) ->
+                io:format("A list was passed (possibly a string):~n");
+            true -> 
+                io:format("Whatever passed is of another type:~n")
+end end,
+    Test(Package_id),
+    io:format(Package_id),
+    io:format("~n"),
+    Test(Location_id),
+    io:format(Location_id),
+    io:format("~n"),
     % SelfPid = self(),
     erpc:call('tpf@business.tpf.markcuizon.com', gen_server, call, [{global, realstorer}, {storing_package, Package_id, Location_id}, infinity]),    % change it out of infinity later
     % erpc:call({realstorer, 'storer@business.tpf.markcuizon.com'}, data_service, store_package, [Package_id, Location_id, SelfPid]),
