a81c68687fe04f548277373046f4ee2a884563c5
marksprojects23 <markc7856@yahoo.com>
2024-03-28 15:45:03 -0600

    moved req2 in location_h just incase

--- tpf_cowboy/src/location_h.erl
+++ tpf_cowboy/src/location_h.erl
@@ -4,7 +4,6 @@
 -export([init/2]).
 
 init(Req, State) ->
-    Req2 = cowboy_req:reply(200, #{}, <<"Location Req">>, Req),
     {ok, Req_body, _End_req} = cowboy_req:read_body(Req),
 
     Decoded_req_body_map = jiffy:decode(Req_body, [return_maps]),
@@ -30,7 +29,7 @@ init(Req, State) ->
             % cast(Node, Module, Function, Args) -> ok
             erpc:cast('tpf@business.tpf.markcuizon.com', gen_server, cast, [{global, realupdater}, {updating_location, Location_id, % #{latitude => Latitude, longitude => Longitude}}]),
             Req_body}]),
-
+            Req2 = cowboy_req:reply(200, #{}, <<"Location Req">>, Req),
             {ok, Req2, State};
         _->
             {ok, Req, State}
